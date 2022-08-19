3ec302d11df3083296393c1995aaab25cc287390
davidson1230 <ddavidson1230@gmail.com>
2022-08-19 15:48:20 -0500

    Added upgrade stuff!!!

--- Upgrades.py
+++ Upgrades.py
@@ -8,8 +8,10 @@ hasGarden = False
 
 def upgradeOption():
     wantUpgrade = input("Would you like to make any upgrades: ")
-    if(wantUpgrade=="yes"):
-        printUpgradeOptions()
+    while(wantUpgrade=="yes"):
+        Upgrades.printUpgradeOptions()
+        Upgrades.chooseUpgrade()
+        wantUpgrade = input("Would you like to make any upgrades: ")
 
 
 def printUpgradeOptions():
@@ -19,7 +21,7 @@ def printUpgradeOptions():
 
 def chooseUpgrade():
     userInput = int(input("Which upgrade: "))
-    while userInput < 1 and userInput > 4:
+    while userInput < 1 and userInput > 5:
         print("Please renenter the number")
         userInput = int(input("Which upgrade: "))
     if userInput == 1:
@@ -60,8 +62,22 @@ def upgradeOxygen():
     else:
         print("Not enough resources")
 def upgradeBase():
-    if(Upgrades.curBaseLevel==1):
+    if(Upgrades.curBaseLevel==1 and Resources.plants >=2 and Resources.bricks >=2):
         Upgrades.curBaseLevel+=1
+        Resources.plants -=2
+        Resources.bricks -=2
+    elif(Upgrades.curBaseLevel ==2 and Resources.plants >=3 and Resources.bricks >=3 and Resources.ore >=1):
+        Upgrades.curBaseLevel +=2
+        Resources.plants -=3
+        Resources.bricks -=3
+        Resources.ore -=1
+    elif(Upgrades.curBaseLevel ==3 and Resources.plants >=5 and Resources.bricks >=5 and Resources.ore >=2):
+        Upgrades.curBaseLevel +=3
+        Resources.plants -=5
+        Resources.bricks -=5
+        Resources.ore -=2
+    else:
+        print("Not enough resources")
 def upgradeFood():
     if(Resources.plants >= 1):
         Resources.food+=1
@@ -69,10 +85,17 @@ def upgradeFood():
     else:
         print("Not enough resources")
 def upgradeFilterGarden():
-    if (curBaseLevel == 1):
-        print("2 Plants and 2 Clay")
-    elif (curBaseLevel == 2):
-        print("3 Plants, 3 Bricks, 1 Ore")
-    elif (curBaseLevel == 3):
-        print("5 Plants, 5 Bricks, 2 Ore")
+    if (curBaseLevel == 2 and Resources.ore >=3 and Resources.plants >=1 ):
+        Resources.ore -=3
+        Resources.plants -=1
+        Upgrades.hasFilter = True
+    elif(hasFilter and Resources.bricks >=3 and Resources.plants >= 5):
+        Resources.bricks -=3
+        Resources.plants -=5
+        Upgrades.hasGarden = True
+    else:
+        print("Not enough resources or base level requirement")
+
+
+
 
