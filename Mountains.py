a5ab7cf28377307cef94b7a4f0c72f77775ae787
Colin <colincharlet4@gmail.com>
2022-08-20 09:35:57 -0500

    Added comments in base and mountains

--- Mountains.py
+++ Mountains.py
@@ -11,22 +11,32 @@ clay = 2
 
 resourceTime = 10
 
+#checks to see if the player has unlocked the bike and changes the travel time based on that
 def getTravelTime():
     if(City.hasBike):
         return travelTime/2
     return travelTime
 
+#if selected takes away oxygen based on the cost of travel
 def returnHome():
     Resources.curOxygen -= getTravelTime()
+
+#if selected adds plants and takes away oxygen
 def harvestPlants():
     Resources.plants+=plant
     Resources.curOxygen -= resourceTime
+
+#if selected adds water and takes away oxygen
 def collectWater():
     Resources.water+=waters
     Resources.curOxygen -= resourceTime
+
+#if selected adds bricks and takes away oxygen
 def digClay():
     Resources.bricks+=clay
     Resources.curOxygen -= resourceTime
+
+#if selected adds ore and takes away oxugen
 def mineOre():
     Resources.ore+=ores
     Resources.curOxygen -= resourceTime
